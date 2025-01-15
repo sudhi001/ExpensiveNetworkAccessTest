35f633d8ac90e2b29330751d93408da05c8d78d2
Sudhi S <sudhis@live.com>
2025-01-15 20:00:50 +0530

    Initial Commit

---
+++ NetworkSpeedTesting/NetworkSpeedTestingApp.swift
@@ -0,0 +1,17 @@
+//
+//  NetworkSpeedTestingApp.swift
+//  NetworkSpeedTesting
+//
+//  Created by Sudhi on 15/01/25.
+//
+
+import SwiftUI
+
+@main
+struct NetworkSpeedTestingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ NetworkSpeedTestingTests/NetworkSpeedTestingTests.swift
@@ -0,0 +1,17 @@
+//
+//  NetworkSpeedTestingTests.swift
+//  NetworkSpeedTestingTests
+//
+//  Created by Sudhi on 15/01/25.
+//
+
+import Testing
+@testable import NetworkSpeedTesting
+
+struct NetworkSpeedTestingTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
